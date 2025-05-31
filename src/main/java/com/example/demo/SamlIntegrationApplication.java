a589a0ab5bf44c00094a1e925fa5e104747fcbb9
Mohish Nilesh Khadse <mohishkhadse3222@gmail.com>
2025-05-31 20:27:47 +0530

    Initial commit_ Simple Saml working flow

---
+++ src/main/java/com/example/demo/SamlIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlIntegrationApplication.class, args);
+	}
+
+}
